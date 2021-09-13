864251c70fd108777e585f5e68ce1195318af4e9
Cyber Hacker <ilgar.hasanzade@gmail.com>
2021-09-13 23:00:27 +0400

    Добавьте файлы проекта.

---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,80 @@
+﻿using Chat.MVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.MVVM.ViewModel
+{
+    class MainViewModel
+    {
+        public ObservableCollection<MessageModel> Messages { get; set; }
+        public ObservableCollection<ContactModel> Contacts { get; set; }
+
+        public MainViewModel()
+        {
+            Messages = new ObservableCollection<MessageModel>();
+            Contacts = new ObservableCollection<ContactModel>();
+
+            Messages.Add(new MessageModel
+            {
+                Username = "Allison",
+                UsernameColor = "#409aff",
+                ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                Message = "Test",
+                Time = DateTime.Now,
+                IsNativeOrigin = false,
+                FirstMessage = true
+            });
+
+            for (int i = 0; i < 3; i++)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "Allison",
+                    UsernameColor = "#409aff",
+                    ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                    Message = "Test",
+                    Time = DateTime.Now,
+                    IsNativeOrigin = false,
+                    FirstMessage = false
+                });
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "Bunny",
+                    UsernameColor = "#409aff",
+                    ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                    Message = "Test",
+                    Time = DateTime.Now,
+                    IsNativeOrigin = true
+                });
+            }
+
+            Messages.Add(new MessageModel
+            {
+                Username = "Bunny",
+                UsernameColor = "#409aff",
+                ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                Message = "Last",
+                Time = DateTime.Now,
+                IsNativeOrigin = true
+            });
+
+            for (int i = 0; i < 5; i++)
+            {
+                Contacts.Add(new ContactModel
+                {
+                    Username = $"Allison {i}",
+                    ImageSource = "https://i.imgur.com/i2szTsp.png",
+                    Messages = Messages
+                });
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chat
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BorderMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed) DragMove();
+        }
+
+        private void btn_Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btn_Maximize_Click(object sender, RoutedEventArgs e)
+        {
+            if(WindowState != WindowState.Maximized) WindowState = WindowState.Maximized;
+            else WindowState = WindowState.Normal;
+        }
+
+        private void btn_Close_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
